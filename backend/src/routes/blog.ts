e27324f69923db6c3a8db93eb824f08895b084ba
abressiddique <abressiddique@gmail.com>
2024-08-14 03:16:52 +0530

    Update blog.ts

--- backend/src/routes/blog.ts
+++ backend/src/routes/blog.ts
@@ -3,6 +3,7 @@ import { PrismaClient } from "@prisma/client/edge";
 import { withAccelerate } from "@prisma/extension-accelerate";
 import { Hono } from "hono";
 import { verify } from "hono/jwt";
+// two main routess 
 
 export const blogRouter = new Hono<{
     Bindings: {
